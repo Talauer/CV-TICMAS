75b533be857e0ffcf626d2f29690f7eb585d7483
Talauer <talauer@yandex.com>
2023-02-28 23:45:46 -0300

    CSS/

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CV-TICMAS
-Cv desarrollado en HTML, CSS y Javascript para la cursada de "Primeros pasos en el Desarrollo Frontend" brindado por TICMAS Academy para el proyecto Argentina Programa 4.0
+Cv desarrollado en HTML, CSS y Javascript para la cursada de "Primeros pasos en el Desarrollo Frontend" brindado por el profesor Daniel Rinaldi en la plataforma TICMAS Academy, dentro de la iniciativa nacional Argentina Programa 4.0
---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+/* el siguiente script lo descarté porque encontré otra función embebida
+mucho más sencilla de aplicar, pero lo dejé para experimentar en una aplicación futura:
+
+<script>
+function confirmacion() {
+	var pregunta = confirm("¿Deseas visitar la página principal?")
+	if (pregunta){
+		alert("Te envío allí rápidamente")
+		window.location = "https://norfipc.com/";
+	}
+	else{
+		alert("Quizás en otro momento...\n Gracias de todas formas")
+	}
+}
+</script>
+<button type="button" onclick="confirmacion()" >Probar confirm</button>*/
